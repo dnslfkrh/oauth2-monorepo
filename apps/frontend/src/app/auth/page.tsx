3234044fdaa1b2bf52be9be467e8e4eee48f8df6
yonguk <lyw3059@gmail.com>
2025-01-15 04:58:09 +0900

    style: one google button

--- apps/frontend/src/app/auth/page.tsx
+++ apps/frontend/src/app/auth/page.tsx
@@ -10,8 +10,6 @@ const LoginPage = () => {
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="flex flex-row items-center space-x-8 p-10 bg-white shadow-xl rounded-xl">
                 <GoogleLoginButton />
-                <GoogleLoginButton />
-                <GoogleLoginButton />
             </div>
         </div>
     );
