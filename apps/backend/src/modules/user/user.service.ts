0510dc9fed39dadcd67cec7342136668f7ddc713
yonguk <lyw3059@gmail.com>
2025-01-15 14:52:58 +0900

    style: compare user type

--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { UserRepository } from "src/repository/user.repository";
+import { userProps } from "src/types/props";
 
 @Injectable()
 export class UserService {
@@ -11,7 +12,7 @@ export class UserService {
         let user = await this.userRepository.findUserByEmail(email);
 
         if (!user) {
-            const newUser = { email, name, picture };
+            const newUser: userProps = { email, name, picture };
             user = await this.userRepository.createUser(newUser);
         }
 
